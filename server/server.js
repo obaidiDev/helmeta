f0b13c98b7823d2cd54dd76dbe52b01f7e4a2f3a
Fadel <fadel.a.alobaidi@gmail.com>
2025-05-07 07:04:01 +0300

    fixing regex error

--- server/server.js
+++ server/server.js
@@ -185,7 +185,7 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('/*', (req, res) => {
+app.get('/*name', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 // --- Socket.IO for browser clients ---
